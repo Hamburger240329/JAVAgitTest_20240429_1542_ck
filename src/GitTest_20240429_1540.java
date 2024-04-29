f55cca7e0db88c7373ff9a597b1a907484aa3ea4
JBEDU29 <5514cyj@gmail.com>
2024-04-29 17:22:30 +0900

    수정사항 업로드

--- src/GitTest_20240429_1540.java
+++ src/GitTest_20240429_1540.java
@@ -4,6 +4,8 @@ public class MyTest_20240429_1540_22 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Helloworld!!");
+		System.out.println("안녕하세요!!");
 	}
 
 }
